e4efa1a9d7100dff8bebed23a16525eb99c65a52
Joe Kaufeld <joe.kaufeld@gmail.com>
2020-02-07 14:32:00 -0500

    moving to other computer
    Signed-off-by: Joe Kaufeld <joe.kaufeld@gmail.com>

---
+++ blossom/slack_conn/strings.py
@@ -0,0 +1,16 @@
+# eventually we'll convert this over to a yaml file or something equally
+# obnoxious to work with but I can't be arsed to do it right now
+
+help_message = """
+Help is on the way!\n\n
+Get information about a user: `@blossom info {username}`\n
+Get general server information: `@blossom info` | `@blossom summary`\n
+Render this message: `@blossom help`
+"""
+
+summary_message = """
+Volunteer count: {0}\n
+Transcription count: {1}\n
+
+How long we've been doing this: {2} years, {3} {4}!
+"""
