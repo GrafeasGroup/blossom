262b05d732a0dc3b070b4d05151c73afbfe51d30
Max van Deursen <M.vanDeursen@student.tudelft.nl>
2020-03-19 17:31:25 +0100

    Fix style errors in blossom.api.authentication

--- api/authentication.py
+++ api/authentication.py
@@ -1,39 +1,47 @@
+"""Module which manages the authentication within the API."""
 import rest_framework.permissions as rfperms
-from django.contrib.auth.models import AnonymousUser
 from django.conf import settings
+from django.contrib.auth.models import AnonymousUser
+from rest_framework.request import Request
+from rest_framework.views import View
+
 
 class AdminApiKeyCustomCheck(rfperms.BasePermission):
+    """Permission check which determines whether the user is a valid admin."""
+
     message = "Sorry, this resource can only be accessed by an admin API key."
 
-    def has_permission(self, request, view):
+    def has_permission(self, request: Request, view: View) -> bool:
+        """
+        Check whether the user is a valid admin.
+
+        :param request: the request which is evaluated
+        :param view: the view to which the request is sent
+        :return: whether the user is a valid admin or not
+        """
         if settings.OVERRIDE_API_AUTH:
             return True
 
         if not isinstance(request.user, AnonymousUser):
             if request.user.api_key:
-                request_key = None
-                if k := request.META.get("Authorization"):
-                    request_key = k
-                elif k := request.headers.get("Authorization"):
-                    request_key = k
+                request_key = request.META.get(
+                    "Authorization", request.headers.get("Authorization", None)
+                )
                 if request_key:
                     request_key = request_key.split()
-                    if len(request_key) == 1:
-                        # they didn't have the Api-Key bit
-                        return
-                else:
-                    return
-
-                return all(
-                    [
-                        request_key[0] == "Api-Key",
-                        request.user.api_key.is_valid(request_key[1]),
-                        request.user.is_grafeas_staff or request.user.is_staff,
-                    ]
-                )
+                    return all(
+                        [
+                            request_key[0] == "Api-Key",
+                            request.user.api_key.is_valid(request_key[1]),
+                            request.user.is_grafeas_staff or request.user.is_staff,
+                        ]
+                    )
+        return False
 
 
 class BlossomApiPermission(rfperms.BasePermission):
+    """Combined check of either the AdminApiKeyCustomCheck or the default Admin Check."""
+
     # For some reason, combining the different auth patterns in the settings
     # file fails miserably and will default to HasAPIKey if it's enabled.
     # I've spent far too long screwing with this, so here's a permission
@@ -42,7 +50,17 @@ class BlossomApiPermission(rfperms.BasePermission):
 
     message = "Sorry, this resource can only be accessed by an admin."
 
-    def has_permission(self, request, view):
+    def has_permission(self, request: Request, view: View) -> bool:
+        """
+        Check whether the user is an admin through either of the two definitions.
+
+        These definitions are determined either through our custom admin check,
+        or the default check supplied by Django REST.
+
+        :param request: the request which is evaluated
+        :param view: the view to which the request is sent
+        :return: whether the user is a valid admin or not
+        """
         return any(
             [
                 rfperms.IsAdminUser().has_permission(request, view),
