13fc4d83e9d4d7528e19798bdfea56e64c0bc3b6
Max van Deursen <M.vanDeursen@student.tudelft.nl>
2020-04-09 16:30:08 +0200

    Reformat helpers and fix style errors in test_auth

---
+++ api/tests/helpers.py
@@ -0,0 +1,80 @@
+"""Helper functions for the API test methods to ease the setup of required instances."""
+from typing import Dict, Tuple
+
+from django.test import Client
+
+from api.models import Submission
+from authentication.models import APIKey, BlossomUser
+
+BASE_SUBMISSION_INFO = {"submission_id": "base_submission_id", "source": "base_source"}
+BASE_USER_INFO = {
+    "username": "base_username",
+    "email": "base_email",
+    "password": "base_password",
+    "is_staff": True,
+    "is_grafeas_staff": True,
+    "is_volunteer": True,
+    "api_key": None,
+}
+
+
+def create_user(**kwargs: object) -> BlossomUser:
+    """
+    Create a new user or get the user with the corresponding user information.
+
+    The user which is created has default fields corresponding to the "BASE_USER_INFO"
+    constant. Any attribute of this constant can be overwritten by providing the attribute
+    with its desired value as a keyword argument (e.g. providing username="test" will
+    result in the returned user to have the username "test").
+
+    :param kwargs: The desired change of fields of the submission compared to the user
+    :returns: The created user
+    """
+    user_info = {
+        key: kwargs.get(key, BASE_USER_INFO[key]) for key in BASE_USER_INFO.keys()
+    }
+    user, _ = BlossomUser.objects.get_or_create(**user_info)
+    return user
+
+
+def create_submission(**kwargs: object) -> Submission:
+    """
+    Create a new submission or get the submission with the corresponding information.
+
+    The submission which is created has default fields corresponding to the
+    "BASE_SUBMISSION_INFO" constant. Any attribute of this constant can be
+    overwritten by providing the attribute with its desired value as a keyword
+    argument (e.g. providing source="test" will result in the returned
+    submission to have the source "test").
+
+
+    :param kwargs: The desired change of fields of the submission compared to the default
+    :returns: The created submission
+    """
+    submission_info = {
+        key: kwargs.get(key, BASE_SUBMISSION_INFO[key])
+        for key in BASE_SUBMISSION_INFO.keys()
+    }
+    return Submission.objects.create(**submission_info)
+
+
+def setup_user_client(
+    client: Client, login: bool = True, **kwargs: object
+) -> Tuple[Client, Dict, BlossomUser]:
+    """
+    Set the client up with a new user, forcing login on the client as a default.
+
+    The user will be created as to the "BASE_USER_INFO" specification. Any
+    desired changes to this user can be provided as a keyword argument to this
+    function.
+
+    :param client: The client to set up
+    :param login: Whether to login the client with the user or not
+    :param kwargs: The desired change of fields of the user compared to the default
+    :returns: The client, the HTTP header with the API key, and the created user
+    """
+    api_key, key = APIKey.objects.create_key(name="base_api_key")
+    user = create_user(api_key=api_key, **kwargs)
+    if login:
+        client.force_login(user)
+    return client, {"HTTP_AUTHORIZATION": f"Api-Key {key}"}, user
