8ea23868bde68a1b44bbf3f4023efd56ccee10d4
Max van Deursen <M.vanDeursen@student.tudelft.nl>
2020-04-10 14:56:41 +0200

    Fix style errors in backends.py

--- authentication/backends.py
+++ authentication/backends.py
@@ -1,15 +1,37 @@
+"""Authentication backend for the application."""
+from typing import Union
+
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth.models import User
 
-# https://stackoverflow.com/a/37332393
+
 class EmailBackend(ModelBackend):
-    def authenticate(self, username=None, password=None, **kwargs):
-        UserModel = get_user_model()
+    """
+    Custom backend which uses the email instead of the username to login.
+
+    Note that this backend is copied from https://stackoverflow.com/a/37332393.
+    """
+
+    def authenticate(
+        self, username: str = None, password: str = None, **kwargs: object
+    ) -> Union[User, None]:
+        """
+        Authenticate the user if proper authentication is provided.
+
+        Note that the username parameter is abused to instead be the email address
+        of the user, which is in turn used to authenticate. This is in line with
+        https://stackoverflow.com/a/37332393.
+
+        :param username: the email address to authenticate with
+        :param password: the password belonging to said email address
+        :param kwargs: possible keyword arguments, although ignored in this method
+        :return: either the corresponding user when authenticated or None otherwise
+        """
+        user_model = get_user_model()
         try:
-            user = UserModel.objects.get(email=username)
-        except UserModel.DoesNotExist:
+            user = user_model.objects.get(email=username)
+        except user_model.DoesNotExist:
             return None
         else:
-            if user.check_password(password):
-                return user
-        return None
+            return user if user.check_password(password) else None
