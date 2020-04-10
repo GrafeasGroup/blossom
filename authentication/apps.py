836e372c26738768812da335068f8480bc9ccd0a
Max van Deursen <M.vanDeursen@student.tudelft.nl>
2020-04-10 14:56:24 +0200

    Fix style errors in apps.py

--- authentication/apps.py
+++ authentication/apps.py
@@ -1,5 +1,7 @@
+"""Configuration for the Authentication application."""
 from django.apps import AppConfig
 
 
 class AuthenticationConfig(AppConfig):
+    """Configuration class for the Authentication application, providing a custom name."""
     name = 'authentication'
