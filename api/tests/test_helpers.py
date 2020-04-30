850b4744ae623ac3bfda707f121cb88cb1ad6dc4
Max van Deursen <M.vanDeursen@student.tudelft.nl>
2020-04-30 15:25:38 +0200

    Merge functionality of Mixins into one BlossomUserMixin (#59)
    * Merge functionality of Mixins into one BlossomUserMixin
    
    * Add tests to the BlossomUserMixin
    
    * Remove errors parameter from mixin method
    
    * Fix style error
    
    * Incorporate feedback
    
    * Remove trailing whitespace

--- api/tests/test_helpers.py
+++ api/tests/test_helpers.py
@@ -1,12 +1,61 @@
 """Test the available helper classes and their methods."""
-from api.helpers import VolunteerMixin
+import pytest
+from rest_framework import status
+from rest_framework.response import Response
+
+from api.helpers import BlossomUserMixin
 from api.tests.helpers import create_user
+from authentication.models import BlossomUser
 
 
-def test_volunteer_mixin_get_username() -> None:
-    """Test whether the Volunteer Mixin provides the correct user given an username."""
-    user = create_user()
-    assert VolunteerMixin().get_volunteer(username=user.username) == user
+@pytest.fixture
+def user() -> BlossomUser:
+    """Pytest fixture to create the standard user before each test."""
+    return create_user()
 
 
-# TODO: Add more tests for the remaining scenarios for both helper classes.
+@pytest.fixture
+def user_mixin() -> BlossomUserMixin:
+    """Pytest fixture to create the BlossomUserMixin before each test."""
+    return BlossomUserMixin()
+
+
+def test_id_retrieval(user: BlossomUser, user_mixin: BlossomUserMixin) -> None:
+    """Test whether the user is correctly retrieved with the user's ID."""
+    result = user_mixin.get_user_from_request({"v_id": user.id})
+    assert result == user
+
+
+def test_username_retrieval(user: BlossomUser, user_mixin: BlossomUserMixin) -> None:
+    """Test whether the user is correctly retrieved with the user's name."""
+    result = user_mixin.get_user_from_request({"username": user.username})
+    assert result == user
+
+
+def test_combined_retrieval(user: BlossomUser, user_mixin: BlossomUserMixin) -> None:
+    """Test whether the user is retrieved when both the ID and username is included."""
+    result = user_mixin.get_user_from_request(
+        {"username": user.username, "v_id": user.id}
+    )
+    assert result == user
+
+
+def test_no_parameters(user_mixin: BlossomUserMixin) -> None:
+    """Test whether a 400 Response is returned when no arguments are passed."""
+    result = user_mixin.get_user_from_request(dict())
+    assert isinstance(result, Response)
+    assert result.status_code == status.HTTP_400_BAD_REQUEST
+
+
+def test_wrong_id(user: BlossomUser, user_mixin: BlossomUserMixin) -> None:
+    """Test whether a 404 Response is returned when an invalid ID is passed."""
+    result = user_mixin.get_user_from_request({"v_id": user.id + 1})
+    assert isinstance(result, Response)
+    assert result.status_code == status.HTTP_404_NOT_FOUND
+
+
+def test_wrong_username(user: BlossomUser, user_mixin: BlossomUserMixin) -> None:
+    """Test whether a 404 Response is returned when an invalid username is passed."""
+    result = user_mixin.get_user_from_request({"username": f"{user.username}404"})
+    assert isinstance(result, Response)
+    assert result.status_code == status.HTTP_404_NOT_FOUND
