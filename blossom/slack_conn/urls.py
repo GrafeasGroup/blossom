6ff867cb79901c4d591212e30ba8f448507f5a1b
Joe Kaufeld <joe.kaufeld@gmail.com>
2020-05-28 09:25:30 -0400

    bLaCk wOuLd mAkE cHaNgEs
    Signed-off-by: Joe Kaufeld <joe.kaufeld@gmail.com>

--- blossom/slack_conn/urls.py
+++ blossom/slack_conn/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from blossom.slack_conn import views
 
 urlpatterns = [
-    path('slack/endpoint/', views.slack_endpoint, name="slack"),
-    path('slack/github/sponsors/', views.github_sponsors_endpoint, name="github_sponsors")
+    path("slack/endpoint/", views.slack_endpoint, name="slack"),
+    path(
+        "slack/github/sponsors/", views.github_sponsors_endpoint, name="github_sponsors"
+    ),
 ]
