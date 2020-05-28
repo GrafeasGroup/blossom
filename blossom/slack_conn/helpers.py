588c3a0ed8dd3299a4740d75d62f5ca5dc92569e
Joe Kaufeld <joe.kaufeld@gmail.com>
2020-05-28 09:29:49 -0400

    Add Github Sponsors Webhook Translation Layer (#71)
    * add github webhook handler
    
    Signed-off-by: Joe Kaufeld <joe.kaufeld@gmail.com>
    
    * minor formatting adjustments
    
    Signed-off-by: Joe Kaufeld <joe.kaufeld@gmail.com>
    
    * address feedback
    
    Signed-off-by: Joe Kaufeld <joe.kaufeld@gmail.com>
    
    * fix linter errors
    
    Signed-off-by: Joe Kaufeld <joe.kaufeld@gmail.com>
    
    * more linter errors fixed
    
    Signed-off-by: Joe Kaufeld <joe.kaufeld@gmail.com>
    
    * even more linter errors!
    
    Signed-off-by: Joe Kaufeld <joe.kaufeld@gmail.com>
    
    * bLaCk wOuLd mAkE cHaNgEs
    
    Signed-off-by: Joe Kaufeld <joe.kaufeld@gmail.com>

--- blossom/slack_conn/helpers.py
+++ blossom/slack_conn/helpers.py
@@ -1,47 +1,70 @@
+import binascii
+import hmac
 import os
 import threading
-from typing import Dict
+from typing import Any, Callable, Dict, List, Tuple, Union
 from unittest import mock
 
 import pytz
 import slack
 from django.conf import settings
+from django.core.handlers.wsgi import WSGIRequest
 from django.utils import timezone
+from slack.web.slack_response import SlackResponse
 
 from api.models import Transcription
-from authentication.models import BlossomUser
 from api.serializers import VolunteerSerializer
+from authentication.models import BlossomUser
 from blossom.strings import translation
 
-if settings.ENABLE_SLACK == True:
-    client = slack.WebClient(token=os.environ['SLACK_API_KEY'])
+if settings.ENABLE_SLACK is True:
+    client = slack.WebClient(token=os.environ["SLACK_API_KEY"])
 else:
     client = mock.Mock()
 
 i18n = translation()
 
 
-# https://stackoverflow.com/a/59043636
-def fire_and_forget(f, *args, **kwargs):
-    def wrapped(*args, **kwargs):
-        threading.Thread(target=f, args=(args), kwargs=kwargs).start()
+def fire_and_forget(
+    func: Callable[[Any], Any], *args: Tuple, **kwargs: Dict
+) -> Callable[[Any], Any]:
+    """
+    Decorate functions to build a thread for a given function and trigger it.
+
+    Originally from https://stackoverflow.com/a/59043636, this function
+    prepares a thread for a given function and then starts it, intentionally
+    severing communication with the thread so that we can continue moving
+    on.
+
+    This should be used sparingly and only when we are 100% sure that
+    the function we are passing does not need to communicate with the main
+    process and that it will exit cleanly (and that if it explodes, we don't
+    care).
+    """
+
+    def wrapped(*args: Tuple, **kwargs: Dict) -> None:
+        threading.Thread(target=func, args=(args), kwargs=kwargs).start()
 
     return wrapped
 
 
-# adapted from
-# https://github.com/varadchoudhari/Neat-Dictionary/blob/master
-# /src/Python%203/neat-dictionary-fixed-column-width.py
-def neat_printer(dictionary, titles, width):
-    return_dict = []
+def neat_printer(dictionary: Dict, titles: List, width: int) -> List:
+    """
+    Take a dict and make it into a tab-separated table.
+
+    Adapted from
+    https://github.com/varadchoudhari/Neat-Dictionary/blob/master
+    /src/Python%203/neat-dictionary-fixed-column-width.py
+    """
+    return_list = []
     formatting = ""
     for i in range(0, len(titles)):
         formatting += "{:<" + str(width) + "}"
         formatting += " | "
     # trim off that last separator
     formatting = formatting[:-3]
-    return_dict.append(formatting.format(*titles))
-    return_dict.append(f"{'-' * width * len(titles)}")
+    return_list.append(formatting.format(*titles))
+    return_list.append(f"{'-' * width * len(titles)}")
     for key, value in dictionary.items():
         sv = []
         if isinstance(value, list):
@@ -51,11 +74,12 @@ def neat_printer(dictionary, titles, width):
             if value is None:
                 value = "None"
             sv = [str(value)]
-        return_dict.append(formatting.format(key, *sv))
-    return return_dict
+        return_list.append(formatting.format(key, *sv))
+    return return_list
 
 
-def get_days():
+def get_days() -> Tuple[Union[int, float], Union[int, float]]:
+    """Return the number of days since the day we opened."""
     # the autoformatter thinks this is evil if it's all on one line.
     # Breaking it up a little for my own sanity.
     start_date = pytz.timezone("UTC").localize(
@@ -65,27 +89,54 @@ def get_days():
     return divmod((timezone.now() - start_date).days, 365)
 
 
-def send_help_message(event):
+def send_help_message(event: Dict) -> None:
+    """Post a help message to slack."""
     client.chat_postMessage(
-        channel=event.get('channel'),
-        text=i18n['slack']['help_message']
+        channel=event.get("channel"), text=i18n["slack"]["help_message"]
     )
 
 
-def send_summary_message(event):
+def send_summary_message(event: Dict) -> None:
+    """Post a summary message to slack."""
     client.chat_postMessage(
-        channel=event.get('channel'),
-        text=i18n['slack']['summary_message'].format(
+        channel=event.get("channel"),
+        text=i18n["slack"]["summary_message"].format(
             BlossomUser.objects.filter(is_volunteer=True).count(),
             Transcription.objects.count(),
             get_days()[0],
             get_days()[1],
-            "days" if get_days()[1] != 1 else "day"
-        )
+            "days" if get_days()[1] != 1 else "day",
+        ),
     )
 
 
-def send_info(event, message):
+def send_github_sponsors_message(data: Dict, action: str) -> None:
+    """
+    Process the POST request from GitHub Sponsors.
+
+    Every time someone performs an action on GitHub Sponsors, we'll get
+    a POST request with the intent and some other information. This translates
+    the GitHub call to something that Slack can understand, then forwards it
+    to the #org_running channel.
+    """
+    emote = ":tada:"
+    if action == "cancelled" or action == "pending_cancellation":
+        emote = ":sob:"
+    if (
+        action == "edited"
+        or action == "tier_changed"
+        or action == "pending_tier_change"
+    ):
+        emote = ":rotating_light:"
+    username = data["sponsorship"]["sponsor"]["login"]
+    sponsorlevel = data["sponsorship"]["tier"]["name"]
+
+    msg = f"{emote} GitHub Sponsors: [{action}] - {username} | {sponsorlevel} {emote}"
+    client.chat_postMessage(channel="org_running", text=msg)
+
+
+def send_info(event: Dict, message: str) -> Union[SlackResponse, None]:
+    """Send info about a user to slack."""
     parsed_message = message.split()
     if len(parsed_message) == 1:
         # they just sent an empty info message
@@ -95,34 +146,34 @@ def send_info(event, message):
         if u := BlossomUser.objects.filter(username__iexact=parsed_message[1]).first():
             v_s = VolunteerSerializer(u, many=False).data
             return client.chat_postMessage(
-                channel=event.get('channel'),
-                text=i18n['slack']['user_info'].format(
+                channel=event.get("channel"),
+                text=i18n["slack"]["user_info"].format(
                     u.username,
                     "\n".join(
                         neat_printer(
                             v_s, ["Key", "Value"], len(max(v_s.keys(), key=len)) + 2
                         )
-                    )
-                )
+                    ),
+                ),
             )
         else:
             return client.chat_postMessage(
-                channel=event.get('channel'),
-                text=i18n['slack']['no_user_by_that_name']
+                channel=event.get("channel"), text=i18n["slack"]["no_user_by_that_name"]
             )
 
     client.chat_postMessage(
-        channel=event.get('channel'), text=i18n['slack']['too_many_params']
+        channel=event.get("channel"), text=i18n["slack"]["too_many_params"]
     )
 
 
-def process_blacklist(event, message):
+def process_blacklist(event: Dict, message: str) -> SlackResponse:
+    """Blacklist a user based on a message from slack."""
     parsed_message = message.split()
     if len(parsed_message) == 1:
         # they didn't give a username
         return client.chat_postMessage(
-            channel=event.get('channel'),
-            text=i18n['slack']['blacklist']['missing_username']
+            channel=event.get("channel"),
+            text=i18n["slack"]["blacklist"]["missing_username"],
         )
     elif len(parsed_message) == 2:
         if u := BlossomUser.objects.filter(username__iexact=parsed_message[1]).first():
@@ -130,48 +181,52 @@ def process_blacklist(event, message):
                 u.blacklisted = False
                 u.save()
                 return client.chat_postMessage(
-                    channel=event.get('channel'),
-                    text=i18n['slack']['blacklist']['success_undo'].format(
+                    channel=event.get("channel"),
+                    text=i18n["slack"]["blacklist"]["success_undo"].format(
                         parsed_message[1]
-                    )
+                    ),
                 )
             else:
                 u.blacklisted = True
                 u.save()
                 return client.chat_postMessage(
-                    channel=event.get('channel'),
-                    text=i18n['slack']['blacklist']['success'].format(parsed_message[1])
+                    channel=event.get("channel"),
+                    text=i18n["slack"]["blacklist"]["success"].format(
+                        parsed_message[1]
+                    ),
                 )
         else:
             return client.chat_postMessage(
-                channel=event.get('channel'),
-                text=i18n['slack']['blacklist']['unknown_username']
+                channel=event.get("channel"),
+                text=i18n["slack"]["blacklist"]["unknown_username"],
             )
     else:
         return client.chat_postMessage(
-            channel=event.get('channel'), text=i18n['slack']['too_many_params']
+            channel=event.get("channel"), text=i18n["slack"]["too_many_params"]
         )
 
 
 @fire_and_forget
 def process_message(data: Dict) -> None:
-    e = data.get('event')
+    """Identify the purpose of a slack message and route accordingly."""
+    e = data.get("event")  # noqa: VNE001
 
     try:
         # What comes in: "<@UTPFNCQS2> hello!"
         # Strip out the beginning section and see what's left.
-        message = e['text'][e['text'].index('>') + 2:].lower()
+        # Note: black and flake8 disagree on the formatting. Black wins.
+        message = e["text"][e["text"].index(">") + 2 :].lower()  # noqa: E203
     except IndexError:
         client.chat_postMessage(
-            channel=e.get('channel'),
-            text="Sorry, something went wrong and I couldn't parse your message."
+            channel=e.get("channel"),
+            text="Sorry, something went wrong and I couldn't parse your message.",
         )
         return
 
     if not message:
         client.chat_postMessage(
-            channel=e.get('channel'),
-            text="Sorry, I wasn't able to get text out of that. Try again."
+            channel=e.get("channel"),
+            text="Sorry, I wasn't able to get text out of that. Try again.",
         )
 
     elif "help" in message:
@@ -188,6 +243,22 @@ def process_message(data: Dict) -> None:
 
     else:
         client.chat_postMessage(
-            channel=e.get('channel'),
-            text="Sorry, I'm not sure what you're asking for."
+            channel=e.get("channel"), text="Sorry, I'm not sure what you're asking for."
         )
+
+
+def is_valid_github_request(request: WSGIRequest) -> bool:
+    """Verify that a webhook from github sponsors is encoded using our key."""
+    if (github_signature := request.headers["x-hub-signature"]) is None:
+        return False
+
+    body_hex = binascii.hexlify(
+        hmac.digest(
+            msg=request.body,
+            key=settings.GITHUB_SPONSORS_SECRET_KEY.encode(),
+            digest="sha1",
+        )
+    ).decode()
+
+    body_hex = f"sha1={body_hex}"
+    return hmac.compare_digest(body_hex, github_signature)
