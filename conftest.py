04b5e6efc034efc72ef7068b4429ca6bb78e2e6b
Joe Kaufeld <joe@kenzie.academy>
2020-08-24 23:38:49 -0400

    I have never met anything as infuriating as flake8

--- conftest.py
+++ conftest.py
@@ -1,13 +1,17 @@
+from typing import Any
+
 import pytest
 
 from blossom.management.commands import bootstrap_site
 
 
 @pytest.fixture(autouse=True)
-def enable_db_access_for_all_tests(db):
+def enable_db_access_for_all_tests(db: Any) -> None:
+    """Give all tests database access."""
     pass
 
 
 @pytest.fixture()
-def setup_site():
+def setup_site() -> None:
+    """Fixture that configures the site as if it were about to be deployed."""
     bootstrap_site.Command().handle()
