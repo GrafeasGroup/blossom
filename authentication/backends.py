fbab18b2a30714f0b45ff3ce68c786bf64dc057c
Joe Kaufeld <joe@kenzie.academy>
2020-09-04 21:42:13 -0400

    make flake8 happy
    Signed-off-by: Joe Kaufeld <joe@kenzie.academy>

--- authentication/backends.py
+++ authentication/backends.py
@@ -1,10 +1,13 @@
 """Authentication backend for the application."""
-from typing import Union
+from typing import Tuple, Union
 
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth.models import User
 from rest_framework import authentication
+from rest_framework.request import Request
+
+from authentication.models import BlossomUser
 
 
 class EmailBackend(ModelBackend):
@@ -39,13 +42,25 @@ class EmailBackend(ModelBackend):
 
 
 class BlossomRestFrameworkAuth(authentication.BaseAuthentication):
-    def authenticate(self, request):
-        if user_data := authentication.SessionAuthentication().authenticate(request):
-            if user_data[0].is_staff or user_data[0].is_grafeas_staff:
-                return user_data
+    def authenticate(self, request: Request) -> Tuple[BlossomUser, bool]:
+        """
+        Handle authentication for the API side.
 
+        DRF needs to handle authentication in a tuple as opposed to a boolean,
+        so it's easier to just expand it into its own type of auth handler.
+
+        The first check is for standard API interactions involving the BlossomAPI
+        class. Username + password. The second check (SessionAuthentication) is
+        used for the web interface -- log in through the front end and browse the
+        API as an authenticated user.
+        """
         if user := EmailBackend().authenticate(
             username=request.data.get("email"), password=request.data.get("password")
         ):
             return (user, None)
+
+        if user_data := authentication.SessionAuthentication().authenticate(request):
+            if user_data[0].is_staff or user_data[0].is_grafeas_staff:
+                return user_data
+
         return None
