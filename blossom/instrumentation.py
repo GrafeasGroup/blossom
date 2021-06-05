cf9b537525f0c7765bdd54d92510f1edc1368cde
David Alexander <opensource@thelonelyghost.com>
2021-06-05 12:02:32 -0400

    Be less clever. Explain why we configure beeline (e.g., local dev)

--- blossom/instrumentation.py
+++ blossom/instrumentation.py
@@ -19,7 +19,10 @@ def post_worker_init(*args: Any, **kwargs: Any) -> None:
     logging.info(f"beeline initialization on process pid {os.getpid()}")
 
     honeycomb_key = os.getenv("HONEYCOMB_KEY", "")
-    beeline.init(
-        writekey=honeycomb_key, dataset="blossom", debug=len(honeycomb_key) == 0
-    )
+    if len(honeycomb_key) == 0:
+        # pass data to honeycomb.io if we have a key
+        beeline.init(writekey=honeycomb_key, dataset="blossom", debug=False)
+    else:
+        # if no api key, do not send data and instead print what would be sent to stderr
+        beeline.init(writekey="", dataset="blossom", debug=True)
     atexit.register(beeline.close)
