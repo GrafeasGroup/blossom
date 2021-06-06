a7ae597c055fe0e4b93590a01722354acd608439
David Alexander <opensource@thelonelyghost.com>
2021-06-06 00:36:35 -0400

    Allow for passing other commands to blossom container image

--- docker/blossom-entrypoint.sh
+++ docker/blossom-entrypoint.sh
@@ -6,6 +6,8 @@ if [ "$1" = "runserver" ]; then
   python manage.py bootstrap_site
   python manage.py collectstatic --noinput -v 0
 
-  gunicorn -c ./blossom/instrumentation.py --access-logfile - --workers 3 --bind "0.0.0.0:${PORT}" blossom.wsgi:application
+  exec gunicorn -c ./blossom/instrumentation.py --access-logfile - --workers 3 --bind "0.0.0.0:${PORT}" blossom.wsgi:application
   # python manage.py runserver "0.0.0.0:${PORT}"
+else
+  exec "$@"
 fi
