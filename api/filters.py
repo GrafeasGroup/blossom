204261b23bab0b3899131a72b3897ca6c081ee9c
Joe Kaufeld <joe.kaufeld@gmail.com>
2021-06-30 05:42:29 -0400

    Add time-based filtering for Submissions (#170)
    * add time-based filtering for submissions
    
    * add additional test
    
    * adjust test to cover last line
    
    Co-authored-by: Joe Kaufeld <joe@kenzie.academy>

---
+++ api/filters.py
@@ -0,0 +1,32 @@
+import dateparser
+from django.db.models.query import QuerySet
+from django.db.models.query_utils import Q
+from django.utils import timezone
+from rest_framework.filters import BaseFilterBackend
+from rest_framework.request import Request
+from rest_framework.views import View
+
+
+class TimeFilter(BaseFilterBackend):
+    def filter_queryset(
+        self, request: Request, queryset: QuerySet, view: View
+    ) -> QuerySet:
+        """Attempt to filter a request by time descriptors if available."""
+        filters = Q()
+        if from_time := request.query_params.get("from"):
+            try:
+                filters = filters & Q(
+                    complete_time__gt=timezone.make_aware(dateparser.parse(from_time))
+                )
+            except AttributeError:
+                pass
+
+        if until_time := request.query_params.get("until"):
+            try:
+                filters = filters & Q(
+                    complete_time__lt=timezone.make_aware(dateparser.parse(until_time))
+                )
+            except AttributeError:
+                pass
+
+        return queryset.filter(filters)
