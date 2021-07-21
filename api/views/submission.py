9d606227092a3a3d96c2a53828e8bd5eb79ed46f
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-07-21 19:23:35 +0200

    Revert "Return user who already claimed a submission"
    This reverts commit df4564be9964d15a41b3b0652a274e200eb463aa.
    
    I accidentally pushed to master. Whoops.

--- api/views/submission.py
+++ api/views/submission.py
@@ -28,7 +28,6 @@ from api.helpers import validate_request
 from api.models import Source, Submission, Transcription
 from api.serializers import SubmissionSerializer
 from api.views.slack_helpers import client as slack
-from api.views.volunteer import VolunteerViewSet
 from authentication.models import BlossomUser
 
 
@@ -274,12 +273,7 @@ class SubmissionViewSet(viewsets.ModelViewSet):
             return Response(status=status.HTTP_403_FORBIDDEN)
 
         if submission.claimed_by is not None:
-            return Response(
-                data=VolunteerViewSet.serializer_class(
-                    submission.claimed_by, context={"request": request}
-                ).data,
-                status=status.HTTP_409_CONFLICT,
-            )
+            return Response(status=status.HTTP_409_CONFLICT)
 
         submission.claimed_by = user
         submission.claim_time = timezone.now()
