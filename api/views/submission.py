c7f582cbb6bab9b0341a06fec119beb03ff4f650
TimJentzsch <tim-jentzsch@gmx.de>
2021-07-28 17:47:09 +0200

    Return user who already claimed a submission (#185)
    When a submission is claimed and then tried to claim again,
    the user who already claimed the submission is returned
    along with an error code.
    
    This is the same as df4564be9964d15a41b3b0652a274e200eb463aa, but
    this time not pushed directly to master.
    
    This reverts commit 9d606227092a3a3d96c2a53828e8bd5eb79ed46f.
    (The revision of the previous commit)

--- api/views/submission.py
+++ api/views/submission.py
@@ -28,6 +28,7 @@ from api.helpers import validate_request
 from api.models import Source, Submission, Transcription
 from api.serializers import SubmissionSerializer
 from api.views.slack_helpers import client as slack
+from api.views.volunteer import VolunteerViewSet
 from authentication.models import BlossomUser
 
 # The maximum number of posts a user can claim
@@ -278,7 +279,12 @@ class SubmissionViewSet(viewsets.ModelViewSet):
             return Response(status=status.HTTP_403_FORBIDDEN)
 
         if submission.claimed_by is not None:
-            return Response(status=status.HTTP_409_CONFLICT)
+            return Response(
+                data=VolunteerViewSet.serializer_class(
+                    submission.claimed_by, context={"request": request}
+                ).data,
+                status=status.HTTP_409_CONFLICT,
+            )
 
         # Determine how many submissions the user has already claimed
         claimed_submissions = Submission.objects.filter(
