8c8e8eae8999ae03e0a0ad1b38bf6e7d46cc2ca1
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-09-01 21:43:35 +0200

    Convert pushshift data to dicts, fix out of range indexes

--- bootstrap/migrate_redis_data.py
+++ bootstrap/migrate_redis_data.py
@@ -1,10 +1,35 @@
-from typing import List
+from datetime import datetime
+from typing import Dict, List, Optional, TypedDict
 
 from psaw import PushshiftAPI
 
 BATCH_SIZE = 20
 
 push = PushshiftAPI()
+
+
+class CommentData(TypedDict):
+    author: str
+    body: str
+    created_utc: datetime
+    id: str
+    link_id: str
+
+
+class SubmissionData(TypedDict):
+    created_utc: datetime
+    id: str
+    permalink: str
+    title: str
+    url: str
+
+
+class DataEntry(TypedDict):
+    done_comment: Optional[CommentData]
+    tor_submission: Optional[SubmissionData]
+    partner_submission: Optional[SubmissionData]
+    tor_comments: Optional[List[CommentData]]
+    transcriptions: Optional[List[CommentData]]
 
 
 def process_done_ids(done_ids: List[str]):
@@ -17,77 +42,64 @@ def process_done_ids(done_ids: List[str]):
 
 
 def process_done_batch(done_ids: List[str]):
-    data = {}
+    comment_filter = ["author", "body", "created_utc", "id", "link_id"]
+    submission_filter = ["created_utc", "id", "permalink", "title", "url"]
+
+    data: Dict[str, DataEntry] = {}
     for done_id in done_ids:
-        data[done_id] = {
-            "done_comment": None,
-            "tor_submission": None,
-            "partner_submission": None,
-            "tor_comments": None,
-            "transcriptions": None,
-        }
+        data[done_id] = {}
 
     # First, do all the calls that we can do in batches
     # 1. Fetch the done comments from their IDs
     print("Fetching dones...")
     done_comments = list(
-        push.search_comments(
-            ids=done_ids,
-            limit=BATCH_SIZE,
-            filter=["author", "body", "created_utc", "id", "link_id"],
-        )
+        push.search_comments(ids=done_ids, limit=BATCH_SIZE, filter=comment_filter,)
     )
+    done_comments = [dict_from_comment(done) for done in done_comments]
     print(done_comments)
     for done_id in data:
-        # The comments are a tuple with the attributes sorted alphabetically...
-        # The id of the comment is index 3
         data[done_id]["done_comment"] = [
-            done for done in done_comments if done[3] == done_id
+            done for done in done_comments if done["id"] == done_id
         ][0]
     # 2. Fetch the ToR submissions from their link id
     print("Fetching ToR submissions...")
-    # done[4] is link_id
-    tor_submission_ids = [done[4] for done in done_comments]
+    tor_submission_ids = [done["link_id"] for done in done_comments]
     print(tor_submission_ids)
     tor_submissions = list(
         push.search_submissions(
-            ids=tor_submission_ids,
-            limit=BATCH_SIZE,
-            filter=["created_utc", "id", "permalink", "url"],
+            ids=tor_submission_ids, limit=BATCH_SIZE, filter=submission_filter,
         )
     )
+    tor_submissions = [dict_from_submission(tor_sub) for tor_sub in tor_submissions]
     print(tor_submissions)
     for done_id in data:
         done = data[done_id]["done_comment"]
-        # tor_sub[1] is id, done[4] is link_id
         data[done_id]["tor_submission"] = [
-            tor_sub for tor_sub in tor_submissions if tor_sub[1] == done[4][3:]
+            tor_sub
+            for tor_sub in tor_submissions
+            if tor_sub["id"] == done["link_id"][3:]
         ][0]
     # 3. Fetch the partner submissions from their url
     print("Fetching partner submissions...")
-    # tor_sub[2] is url
-    partner_submission_urls = [tor_sub[2] for tor_sub in tor_submissions]
+    partner_submission_urls = [tor_sub["url"] for tor_sub in tor_submissions]
     partner_submission_ids = [
         extract_submission_id_from_url(url) for url in partner_submission_urls
     ]
     partner_submissions = list(
         push.search_submissions(
-            ids=partner_submission_ids,
-            limit=BATCH_SIZE,
-            filter=["created_utc", "id", "permalink", "title", "url"],
+            ids=partner_submission_ids, limit=BATCH_SIZE, filter=submission_filter,
         )
     )
+    partner_submissions = [
+        dict_from_submission(partner_sub) for partner_sub in partner_submissions
+    ]
     print(partner_submissions)
     for done_id in data:
         tor_sub = data[done_id]["tor_submission"]
-        # p_sub[1] is id, tor_sub[3] is url
-        print(partner_submissions[0][1])
-        print(tor_sub[3])
-        print(extract_submission_id_from_url(tor_sub[3]))
         data[done_id]["partner_submission"] = [
             p_sub
             for p_sub in partner_submissions
-            if p_sub[1] == extract_submission_id_from_url(tor_sub[3])
+            if p_sub["id"] == extract_submission_id_from_url(tor_sub["url"])
         ][0]
 
     # Next we need to do the individual calls
@@ -95,30 +107,46 @@ def process_done_batch(done_ids: List[str]):
     print("Fetching ToR comments...")
     for done_id in data:
         done = data[done_id]["done_comment"]
-        # done[4] is link_id
         tor_comments = list(
-            push.search_comments(
-                link_id=done[4], filter=["author", "body", "created_utc", "link_id"]
-            )
+            push.search_comments(link_id=done["link_id"], filter=comment_filter)
         )
-        data["tor_comments"] = tor_comments
+        tor_comments = [dict_from_comment(tor_com) for tor_com in tor_comments]
+        data[done_id]["tor_comments"] = tor_comments
     # 2. Get transcription comments on the partner submission
     print("Fetching transcriptions...")
     for done_id in data:
         done = data[done_id]["done_comment"]
         p_sub = data[done_id]["partner_submission"]
-        # p_sub[1] is id, done[0] is author
         transcriptions = list(
             push.search_comments(
-                link_id=p_sub[1],
-                author=done[0],
-                filter=["author", "body", "created_utc", "link_id"],
+                link_id=p_sub["id"], author=done["author"], filter=comment_filter,
             )
         )
-        data["transcriptions"] = transcriptions
+        transcriptions = [dict_from_comment(tr) for tr in transcriptions]
+        data[done_id]["transcriptions"] = transcriptions
 
     print("DONE")
     print(data)
+
+
+def dict_from_comment(comment) -> CommentData:
+    return {
+        "author": comment[0],
+        "body": comment[1],
+        "created_utc": datetime.utcfromtimestamp(comment[2]),
+        "id": comment[3],
+        "link_id": comment[4],
+    }
+
+
+def dict_from_submission(submission) -> SubmissionData:
+    return {
+        "created_utc": datetime.utcfromtimestamp(submission[0]),
+        "id": submission[1],
+        "permalink": submission[2],
+        "title": submission[3],
+        "url": submission[4],
+    }
 
 
 def extract_submission_id_from_url(url: str) -> str:
