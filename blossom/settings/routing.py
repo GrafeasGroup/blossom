eec702eee4eae3c328e042e71399242a6648738e
Joe Kaufeld <joe.kaufeld@gmail.com>
2021-11-12 13:09:22 -0500

    Remove need for settings flag on all commands locally (#211)
    * remove need for settings flag on all commands locally
    
    * undo accidental additional change to readme

--- blossom/settings/routing.py
+++ blossom/settings/routing.py
@@ -1,13 +1,21 @@
+# noqa: F401,F403
+
+import logging
 import os
 
 # Route us to the correct settings file based on environment variables. Allows
 # us to add a stage environment really easily.
 
+logger = logging.getLogger("blossom")
 env = os.environ.get("ENVIRONMENT", None)
 
 if env == "local":
     from blossom.settings.local import *
 elif env == "testing":
     from blossom.settings.testing import *
+elif os.path.exists("local_settings.py"):
+    # Local override -- check for existence of local_settings.py and load it if possible
+    logger.warning("Found local_settings.py -- loading and using!")
+    from local_settings import *
 else:
     from blossom.settings.prod import *
