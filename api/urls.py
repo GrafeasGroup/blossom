29f39af54bc5ef4685b094b97c8ae04f95ade35c
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-12-18 22:14:37 +0100

    Fix name of find endpoint

--- api/urls.py
+++ api/urls.py
@@ -75,7 +75,7 @@ router.register(r"source", source.SourceViewSet, basename="source")
 urlpatterns = [
     url(r"", include(router.urls)),
     url(r"^summary/", misc.SummaryView.as_view(), name="summary"),
-    url(r"^find/", find.FindView.as_view(), name="summary"),
+    url(r"^find/", find.FindView.as_view(), name="find"),
     url(
         r"^swagger(?P<format>\.json|\.yaml)$",
         schema_view.without_ui(cache_timeout=0),
