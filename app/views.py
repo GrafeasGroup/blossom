8b451e6978f016bac44dc918a3d3e075246583e5
Joe Kaufeld <joe.kaufeld@gmail.com>
2021-12-20 12:28:49 -0500

    update footer and fix formatting issues

--- app/views.py
+++ app/views.py
@@ -58,11 +58,10 @@ EXCITEMENT = [
 
 TRANSCRIPTION_TEMPLATE = (
     "Image Transcription: {content_type}\n\n---\n\n{transcription}\n\n---\n\n"
-    "^^I'm&#32;a&#32;human&#32;volunteer&#32;content&#32;transcriber&#32;"
-    "for&#32;Reddit&#32;and&#32;you&#32;could&#32;be&#32;too!&#32;"
-    "[If&#32;you'd&#32;like&#32;more&#32;information&#32;on&#32;what&#32;"
-    "we&#32;do&#32;and&#32;why&#32;we&#32;do&#32;it,&#32;click&#32;here!]"
-    "(https://www.reddit.com/r/TranscribersOfReddit/wiki/index)"
+    "^^I'm&#32;a&#32;human&#32;volunteer&#32;content&#32;transcriber&#32;and&#32;"
+    "you&#32;could&#32;be&#32;too!&#32;[If&#32;you'd&#32;like&#32;more&#32;"
+    "information&#32;on&#32;what&#32;we&#32;do&#32;and&#32;why&#32;we&#32;do&#32;"
+    "it,&#32;click&#32;here!](https://www.reddit.com/r/TranscribersOfReddit/wiki/index)"
 )
 
 
@@ -368,7 +367,7 @@ def ask_about_removing_post(request: HttpRequest, submission: Submission) -> Non
                 {
                     "type": "button",
                     "style": "danger",
-                    "text": {"type": "plain_text", "text": "Remove",},  # noqa: E231
+                    "text": {"type": "plain_text", "text": "Remove"},
                     "value": "remove_submission_{}",
                     "confirm": {
                         "title": {"type": "plain_text", "text": "Are you sure?"},
