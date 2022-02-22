37e51c5737e0705a96a27d5c8ca2beab3c2986a2
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-02-22 16:26:35 +0100

    Patch Slack calls instead of overwriting them

--- api/tests/submissions/test_submission_report.py
+++ api/tests/submissions/test_submission_report.py
@@ -1,11 +1,10 @@
 import json
-from unittest.mock import MagicMock
+from unittest.mock import patch
 
 from django.test import Client
 from django.urls import reverse
 from rest_framework import status
 
-from api.slack import client as slack_client
 from utils.test_helpers import create_submission, setup_user_client
 
 
@@ -14,7 +13,6 @@ class TestSubmissionReport:
 
     def test_report_already_removed(self, client: Client) -> None:
         """Verify that reporting an already removed submission doesn't do anything."""
-        slack_client.chat_postMessage = MagicMock()
         client, headers, user = setup_user_client(client)
 
         submission = create_submission(id=3, removed_from_queue=True)
@@ -22,23 +20,22 @@ class TestSubmissionReport:
 
         data = {"reason": "Violation of ALL the rules"}
 
-        result = client.patch(
-            reverse("submission-report", args=[submission.id]),
-            json.dumps(data),
-            content_type="application/json",
-            **headers
-        )
+        with patch("api.slack.client.chat_postMessage") as mock:
+            result = client.patch(
+                reverse("submission-report", args=[submission.id]),
+                json.dumps(data),
+                content_type="application/json",
+                **headers
+            )
 
-        submission.refresh_from_db()
+            submission.refresh_from_db()
 
-        assert result.status_code == status.HTTP_201_CREATED
-        assert submission.removed_from_queue
-        assert slack_client.chat_postMessage.call_count == 0
+            assert result.status_code == status.HTTP_201_CREATED
+            assert submission.removed_from_queue
+            assert mock.call_count == 0
 
     def test_report_not_removed(self, client: Client) -> None:
         """Verify that reporting sends a message to Slack."""
-        mock = MagicMock()
-        slack_client.chat_postMessage = mock
         client, headers, user = setup_user_client(client)
 
         submission = create_submission(id=3)
@@ -49,12 +46,13 @@ class TestSubmissionReport:
 
         data = {"reason": "Violation of ALL the rules"}
 
-        result = client.patch(
-            reverse("submission-report", args=[submission.id]),
-            json.dumps(data),
-            content_type="application/json",
-            **headers
-        )
+        with patch("api.slack.client.chat_postMessage"):
+            result = client.patch(
+                reverse("submission-report", args=[submission.id]),
+                json.dumps(data),
+                content_type="application/json",
+                **headers
+            )
 
         submission.refresh_from_db()
 
