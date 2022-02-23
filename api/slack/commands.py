1a300ef26d277dbdeac1ea4dec04432104019f7c
TimJentzsch <tim-jentzsch@gmx.de>
2022-02-23 16:04:00 +0100

    Add watchstatus Slack command (#364)
    * Add watchstatus Slack command
    
    * Add tests for watchstatus command
    
    * Add watchstatus command to help output

--- api/slack/commands.py
+++ api/slack/commands.py
@@ -45,6 +45,7 @@ def process_command(data: Dict) -> None:
         "blacklist": blacklist_cmd,
         "watch": watch_cmd,
         "unwatch": unwatch_cmd,
+        "watchstatus": watchstatus_cmd,
         "watchlist": watchlist_cmd,
         "dadjoke": dadjoke_cmd,
     }
@@ -200,6 +201,29 @@ def unwatch_cmd(channel: str, message: str) -> None:
             user.save()
 
             msg = i18n["slack"]["unwatch"]["success"].format(user=user.username)
+        else:
+            msg = i18n["slack"]["errors"]["unknown_username"]
+
+    else:
+        msg = i18n["slack"]["errors"]["too_many_params"]
+
+    client.chat_postMessage(channel=channel, text=msg)
+
+
+def watchstatus_cmd(channel: str, message: str) -> None:
+    """Get the watch status of a specific user."""
+    parsed_message = message.split()
+
+    if len(parsed_message) == 1:
+        # they didn't give a username
+        msg = i18n["slack"]["errors"]["missing_username"]
+    elif len(parsed_message) == 2:
+        username = clean_links(parsed_message[1])
+        if user := BlossomUser.objects.filter(username__iexact=username).first():
+            status = user.transcription_check_reason(ignore_low_activity=True)
+            msg = i18n["slack"]["watchstatus"]["success"].format(
+                user=user.username, status=status
+            )
         else:
             msg = i18n["slack"]["errors"]["unknown_username"]
 
--- api/tests/test_slack.py
+++ api/tests/test_slack.py
@@ -21,6 +21,7 @@ from api.slack.commands import (
     unwatch_cmd,
     watch_cmd,
     watchlist_cmd,
+    watchstatus_cmd,
 )
 from api.slack.utils import dict_to_table, send_transcription_check
 from api.views.slack import github_sponsors_endpoint
@@ -541,6 +542,45 @@ def test_process_unwatch_error(message: str, response: str) -> None:
     test_user.refresh_from_db()
 
     assert test_user.overwrite_check_percentage is None
+    assert slack_client.chat_postMessage.call_args[1]["text"] == response
+
+
+def test_process_watchstatus() -> None:
+    """Test watchstatus functionality."""
+    slack_client.chat_postMessage = MagicMock()
+
+    test_user = create_user(username="u123", overwrite_check_percentage=0.5)
+    assert test_user.overwrite_check_percentage == 0.5
+
+    # process the message
+    watchstatus_cmd("", "watchstatus u123")
+    slack_client.chat_postMessage.assert_called_once()
+    test_user.refresh_from_db()
+    expected_message = i18n["slack"]["watchstatus"]["success"].format(
+        user=test_user.username, status="Watched (50.0%)"
+    )
+
+    assert slack_client.chat_postMessage.call_args[1]["text"] == expected_message
+
+
+@pytest.mark.parametrize(
+    "message,response",
+    [
+        ("watchstatus", i18n["slack"]["errors"]["missing_username"]),
+        ("watchstatus u123 50", i18n["slack"]["errors"]["too_many_params"]),
+    ],
+)
+def test_process_watchstatus_error(message: str, response: str) -> None:
+    """Test watch command for invalid messages."""
+    slack_client.chat_postMessage = MagicMock()
+
+    test_user = create_user(username="u123")
+
+    # process the message
+    watchstatus_cmd("", message)
+    slack_client.chat_postMessage.assert_called_once()
+    test_user.refresh_from_db()
+
     assert slack_client.chat_postMessage.call_args[1]["text"] == response
 
 
