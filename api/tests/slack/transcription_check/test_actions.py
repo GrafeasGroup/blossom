818b7e78f22f9ec07ed662388ac0fac61f56d5a0
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-02-26 17:05:15 +0100

    Add tests for claiming and unclaiming transcription checks

--- api/tests/slack/transcription_check/test_actions.py
+++ api/tests/slack/transcription_check/test_actions.py
@@ -1,5 +1,9 @@
+from datetime import datetime
+from typing import Optional
+
 import pytest
 from django.test import Client
+from django.utils import timezone
 
 from api.models import TranscriptionCheck
 from api.slack.transcription_check.actions import _update_db_model
@@ -12,6 +16,18 @@ from utils.test_helpers import (
 )
 
 CheckStatus = TranscriptionCheck.TranscriptionCheckStatus
+
+
+def _is_time_recent(start: datetime, time: Optional[datetime]) -> bool:
+    """Determine whether the given time indicates the current time.
+
+    :param start: The start time before the given time was created.
+    :param time: The time to check.
+    """
+    if time is None:
+        return False
+
+    return start <= time <= timezone.now()
 
 
 @pytest.mark.parametrize(
@@ -50,3 +66,38 @@ def test_update_db_model_status(
     check.refresh_from_db()
 
     assert check.status == post_status
+
+
+def test_update_db_model_claim(client: Client) -> None:
+    """Test the updating of the check DB model after claiming."""
+    client, headers, user = setup_user_client(client, id=100, username="Userson")
+    mod = create_user(id=200, username="Moddington")
+    submission = create_submission(claimed_by=user, completed_by=user)
+    transcription = create_transcription(submission=submission, user=user)
+    check = create_check(transcription)
+    assert check.moderator is None
+    assert check.claim_time is None
+
+    start = timezone.now()
+    assert _update_db_model(check, mod, "claim")
+    check.refresh_from_db()
+
+    assert check.moderator == mod
+    assert _is_time_recent(start, check.claim_time)
+
+
+def test_update_db_model_unclaim(client: Client) -> None:
+    """Test the updating of the check DB model after unclaiming."""
+    client, headers, user = setup_user_client(client, id=100, username="Userson")
+    mod = create_user(id=200, username="Moddington")
+    submission = create_submission(claimed_by=user, completed_by=user)
+    transcription = create_transcription(submission=submission, user=user)
+    check = create_check(transcription, moderator=mod, claim_time=timezone.now())
+    assert check.moderator == mod
+    assert check.claim_time is not None
+
+    assert _update_db_model(check, mod, "unclaim")
+    check.refresh_from_db()
+
+    assert check.moderator is None
+    assert check.claim_time is None
